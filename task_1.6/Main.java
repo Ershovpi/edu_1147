0769438af7849cd18691528ea0fa08b3de28aae1
Ershovpi <90197304+Ershovpi@users.noreply.github.com>
2021-09-06 19:18:09 +0300

    Update Main.java

--- task_1.6/Main.java
+++ task_1.6/Main.java
@@ -2,8 +2,12 @@
 Пользователь вводит число, необходимо увеличить это число на 15% и вывести результат на экран
 */
 
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        // Ваш код
+        Scanner scanner = new Scanner (System.in);
+        System.out.print("Введите число: ");
+        float num = scanner.nextFloat();
+        System.out.print("+15% = "+num*1.15);
     }
 }
