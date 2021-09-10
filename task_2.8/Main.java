51923a73f9e86ab6ed64369f9c3d45b0b851cf99
Ershovpi <90197304+Ershovpi@users.noreply.github.com>
2021-09-10 15:09:56 +0300

    Update Main.java

--- task_2.8/Main.java
+++ task_2.8/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         int[] a = {2, 3, 4, 5};
         int p = 1;
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < a.length; i++) {
             p = p * a[i];
         }
         System.out.println(p);
