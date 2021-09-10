16be702a31d1812f4ce4486de9bfdf473876773b
Ershovpi <90197304+Ershovpi@users.noreply.github.com>
2021-09-10 10:36:58 +0300

    Update Main.java

--- task_2.4/Main.java
+++ task_2.4/Main.java
@@ -4,7 +4,15 @@
 
 public class Main {
     public static void main(String[] args) {
-        //напишите тут ваш код
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите сумму вклада: ");
+        double summ = scanner.nextDouble();
+        System.out.print("Введите годовой процент: ");
+        int p = scanner.nextInt();
+        System.out.println("Сумма вклада "+summ+", под "+p+"% годовых");
+        for (int i=0; i<5; i++) {
+            summ = summ + (summ * p / 100);
+            System.out.println(summ);
+        }
     }
 }
