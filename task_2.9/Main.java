4831f9052448b5a5a2cb177c707bae4e5ddfb7e3
Ershovpi <90197304+Ershovpi@users.noreply.github.com>
2021-09-10 11:45:13 +0300

    Update Main.java

--- task_2.9/Main.java
+++ task_2.9/Main.java
@@ -4,7 +4,20 @@
 
 public class Main {
     public static void main(String[] args) {
-        //напишите тут ваш код
-
+       Scanner scan = new Scanner(System.in);
+        System.out.println("Введите массив из 4 элементов: ");
+        int [] mas = {0,0,0,0};
+        Boolean b = false;
+        for (int i = 0; i < 4; i++) {
+            mas[i] = scan.nextInt();
+            if (mas[i]==5) {
+                b = true;
+            }
+        }
+        if (b) {
+            System.out.println("да");
+        } else {
+            System.out.println("нет");
+        }
     }
 }
