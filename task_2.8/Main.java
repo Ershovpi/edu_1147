397042ff2e6de1cf07c83c5dcece1b5af1349947
Ershovpi <90197304+Ershovpi@users.noreply.github.com>
2021-09-10 11:13:33 +0300

    Update Main.java

--- task_2.8/Main.java
+++ task_2.8/Main.java
@@ -4,7 +4,11 @@
 
 public class Main {
     public static void main(String[] args) {
-        //напишите тут ваш код
-
+        int[] a = {2, 3, 4, 5};
+        int p = 1;
+        for (int i = 0; i < 4; i++) {
+            p = p * a[i];
+        }
+        System.out.println(p);
     }
 }
