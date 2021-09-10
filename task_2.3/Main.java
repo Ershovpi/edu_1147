07d4ae5d55e095104a2498bab36ab2f20a100886
Ershovpi <90197304+Ershovpi@users.noreply.github.com>
2021-09-10 10:20:18 +0300

    Update Main.java

--- task_2.3/Main.java
+++ task_2.3/Main.java
@@ -15,7 +15,11 @@
 
 public class Main {
     public static void main(String[] args) {
-        //напишите тут ваш код
-
+        for (int a = 1; a < 11; a++) {
+            for (int b = 0; b < a; b++) {
+                System.out.print("8");
+            }
+            System.out.println();
+        }
     }
 }
