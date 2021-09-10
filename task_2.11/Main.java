5fa6e04b42fbd29e254cb57132d14ae16d93a16b
Ershovpi <90197304+Ershovpi@users.noreply.github.com>
2021-09-10 12:16:33 +0300

    Update Main.java

--- task_2.11/Main.java
+++ task_2.11/Main.java
@@ -4,7 +4,22 @@
 
 public class Main {
     public static void main(String[] args) {
-        //напишите тут ваш код
-
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите массив из 5 элементов: ");
+        int [] mas = {0,0,0,0,0};
+        int summ = 0;
+        int n = 1;
+        for (int i = 0; i < 5; i++) {
+            mas[i] = scan.nextInt();
+            summ = summ + mas[i];
+            if (summ<=10) {
+                n++;
+            }
+        }
+        if (n>5) {
+            System.out.println("Сумма элементов не больше 10");
+        } else {
+            System.out.println(n+" элемента (ов) массива > 10");
+        }
     }
 }
