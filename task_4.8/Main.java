dc41baca26a02da9301d099881f7f114ff883f99
Ershovpi <90197304+Ershovpi@users.noreply.github.com>
2021-09-18 17:03:52 +0300

    Update Main.java

--- task_4.8/Main.java
+++ task_4.8/Main.java
@@ -1,19 +1,19 @@
 import java.util.Scanner;
 
 /*
-*  ***Гостиница***
-*  1) Гостиница
-*  2) Номера (кол-во мест(1-3), сан.узел(есть/нет), питаение(есть/нет), wifi(есть/нет), свободен/занят)
-*
-*   *Задание для лабораторной работы №4*
-*   Освободить комнату
-*   Вывести свойства комнаты
-*   Показать комнаты с WiFi
-*   Показать комнаты с WC
-*   Показать комнаты с Eat
-*   Показать комнаты по кол-ву спальных мест
-*   Отобразить список команд
-* */
+ *  ***Гостиница***
+ *  1) Гостиница
+ *  2) Номера (кол-во мест(1-3), сан.узел(есть/нет), питание(есть/нет), wifi(есть/нет), свободен/занят)
+ *
+ *   *Задание для лабораторной работы №4*
+ *   Освободить комнату
+ *   Вывести свойства комнаты
+ *   Показать комнаты с WiFi
+ *   Показать комнаты с WC
+ *   Показать комнаты с Eat
+ *   Показать комнаты по кол-ву спальных мест
+ *   Отобразить список команд
+ * */
 public class Main {
     public static void main(String[] args) {
         Room[] rooms = {
@@ -35,12 +35,48 @@ public class Main {
             command = scanner.nextLine();
             if(command.equals("getFreeRooms")){
                 hotel.getFreeRooms();
-            }else if(command.equals("reserveRoom")){
+            }else if (command.equals("getWifiRooms")){
+                hotel.getWifiRooms();
+            }else if (command.equals("getWcRooms")){
+                hotel.getWcRooms();
+            }else if (command.equals("getEatRooms")){
+                hotel.getEatRooms();
+            }else if (command.equals("getSleepRooms")){
+                System.out.print("Введите количество спальных комнат: ");
+                byte roomQuantity = (byte) scanner.nextInt();
+                hotel.getSleepRooms(roomQuantity);
+            }else if (command.equals("getRoomFeatures")){
+                System.out.print("Введите номер комнаты: ");
+                byte roomNumber = (byte) scanner.nextInt();
+                hotel.getRoomFeatures(roomNumber);
+            }else if(command.equals("reserveRoom")) {
                 System.out.print("Введите номер комнаты для бронирования: ");
                 byte roomNumber = (byte) scanner.nextInt();
                 hotel.reserveRoom(roomNumber);
+            }else if(command.equals("freeRoom")){
+                System.out.print("Введите номер освобождаемой комнаты: ");
+                byte roomNumber = (byte) scanner.nextInt();
+                hotel.freeRoom(roomNumber);
             }else if (command.equals("exit")){
                 break;
+            }else if (command.equals("")){
+
+            }else {
+                if (!command.equals("help"))
+                    System.out.println("Неверная команда. Список возможных команд:");
+                String help = "*----*\n" +
+                        "getFreeRooms - показать все свободные номера\n" +
+                        "getWifiRooms - показать все номера c WiFi\n"+
+                        "getWcRooms - показать все номера c туалетом\n"+
+                        "getEatRooms - показать все номера c едой\n"+
+                        "getSleepRooms - показать все номера c указанным количеством комнат\n"+
+                        "getRoomFeatures - показать свойства номера\n"+
+                        "reserveRoom - забронировать номер\n" +
+                        "freeRoom - забронировать номер\n" +
+                        "exit - выход из программы\n" +
+                        "help - вывод подсказки по командам\n" +
+                        "*----*";
+                System.out.println(help);
             }
         }
 
