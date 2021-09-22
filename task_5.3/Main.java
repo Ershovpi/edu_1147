622fde44db14ebc6592d4b6e590cbc3499bdd5c0
Ershovpi <90197304+Ershovpi@users.noreply.github.com>
2021-09-22 15:49:24 +0300

    Update Main.java

--- task_5.3/Main.java
+++ task_5.3/Main.java
@@ -6,6 +6,25 @@
 import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
-    
+        ArrayList list = new ArrayList();
+        ArrayList newList = new ArrayList();
+        list.add(4);
+        list.add(-1);
+        list.add(-5);
+        list.add(2);
+        list.add(7);
+        list.add(13);
+        list.add(-7);
+        list.add(0);
+        list.add(3);
+        list.add(6);
+        list.add(18);
+        list.add(9);
+        list.add(-4);
+        for (int i = 0; i < list.size(); i++)
+            if((int)(list.get(i))>0 && (int)(list.get(i))<10) {
+                newList.add(list.get(i));
+                System.out.print(list.get(i)+" ");
+            }
     }
 }
