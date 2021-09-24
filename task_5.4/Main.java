f5f053c41053237adad33d79e4cbb8716c7dc6d0
Ershovpi <90197304+Ershovpi@users.noreply.github.com>
2021-09-24 11:02:48 +0300

    Update Main.java

--- task_5.4/Main.java
+++ task_5.4/Main.java
@@ -19,8 +19,38 @@
 10
 */
 
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.io.*;
+
 public class Main {
-    public static void main(String[] args) {
-        // напишите тут ваш код
+    public static void main(String[] args) throws IOException{
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите имя файла: ");
+        String file = scanner.nextLine();
+        try {
+            FileInputStream fin = new FileInputStream(file);
+            int i;
+            String number = "";
+            while ((i=fin.read())!=-1) {
+                if(i!=13) number = number + (char)i;
+                else {
+                    fin.read();
+                    if(Integer.parseInt(number)%2==0)
+                        list.add(Integer.parseInt(number));
+                    number = "";
+                    }
+            }
+            list.add(Integer.parseInt(number));
+            fin.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл отсутствует");
+        }
+        Collections.sort(list);
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
     }
 }
