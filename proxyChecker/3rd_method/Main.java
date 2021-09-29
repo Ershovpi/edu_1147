f8b3935cc444a09e51b20774c25918e672eff335
Ershovpi <90197304+Ershovpi@users.noreply.github.com>
2021-09-30 00:32:01 +0300

    Create Main.java

---
+++ proxyChecker/3rd_method/Main.java
@@ -0,0 +1,70 @@
+import java.io.*;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            FileInputStream fin = new FileInputStream("C:/java/ip.txt");
+            int i;
+            String result = "";
+
+            while ((i = fin.read()) != -1){
+                if(i==13) continue; // Если возврат каретки, то пропускаем итерацию
+                else if(i == 10){ // Перенос строки
+                    String ip = result.split(":")[0];
+                    int port = Integer.parseInt(result.split(":")[1]);
+                    result = "";
+                    Thread thread = new Thread(new Runnable() {
+                        private void checkProxy(String ip, int port){
+                            try {
+                                // Создаём объект прокси
+                                Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(ip,port));
+                                // Указываем, к какому ресурсу нужно подключиться, данный ресурс https://vozhzhaev.ru/test.php
+                                // в ответ всегда отдаст вам ip
+                                URL url = new URL("https://vozhzhaev.ru/test.php");
+                                // Открываем соединение
+                                URLConnection URLConnection = url.openConnection(proxy);
+                                // Сохраняем ответ в поток ввода
+                                BufferedReader in = new BufferedReader(
+                                        new InputStreamReader(URLConnection.getInputStream())
+                                );
+                                // Переменная, в которую в формате строки кладём ответ от сервера
+                                String inputLine;
+                                // Читаем ответ от сервера, пока не встретится null
+                                while ((inputLine = in.readLine()) != null) {
+                                    System.out.println("IP: " + inputLine + " - работает!");
+                                    String outputText = ip + ":" + port+"\n";
+                                    byte [] buffer = outputText.getBytes();
+                                    FileOutputStream fos = new FileOutputStream("c:/java/good_ip.txt", true);
+                                    fos.write(buffer);
+                                    fos.close();
+                                }
+                                // Закрываем поток ввода
+                                in.close();
+
+                            } catch (IOException e) {
+                                // Если ip не доступен, то будет Exception, но мы его обрабатываем и выводим инфо на экран
+                                System.out.println("IP: "+ip+" не работает!");
+                            }
+                        }
+
+                        @Override
+                        public void run() {
+                            checkProxy(ip, port);
+                        }
+                    });
+                    thread.start();
+                }else if(i==9) { // Табуляция
+                    result += ":";
+                }else{ // Остальные символы
+                    result += (char) i;
+                }
+            }
+            }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
